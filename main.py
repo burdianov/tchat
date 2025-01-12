0f8458c47e0a25d560f0b7bfa119bb83d2d0e190
Lilian Burdianov <lilian.burdianov@gmail.com>
2025-01-13 00:26:52 +0400

    add chain

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+
+chat = ChatOpenAI(model="gpt-4o-mini")
+
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[HumanMessagePromptTemplate.from_template("{content}")],
+)
+
+chain = RunnableSequence(prompt, chat)
+
 while True:
     content = input(">> ")
 
-    print(f"You entered: {content}")
+    result = chain.invoke({"content": content})
+
+    print(result.content)
